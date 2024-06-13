58bd02a91a6301aae1919801d1f23f4e726a3fab
Priya <>
2024-06-13 19:05:19 -0400

    added test file

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static String a = "hello";
+    public static String b = "world";
+
+    public static void main(String[] args) {
+        System.out.println(a+" "+b);
+    }
+
+}
